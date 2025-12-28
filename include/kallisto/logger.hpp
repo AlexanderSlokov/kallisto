6044b7e1f4a1ff794a69e0d5c0e60fe87492ef7b
Alexander Slokhov <102212788+AlexanderSlokov@users.noreply.github.com>
2025-12-28 11:41:58 +0700

    Update logger.hpp

--- include/kallisto/logger.hpp
+++ include/kallisto/logger.hpp
@@ -1,3 +1,5 @@
+// Inspired by HashiCorp Vault's logger design
+
 #pragma once // Thay cho Include Guard truyền thống
 
 #include <string>
@@ -9,22 +11,22 @@ namespace kallisto {
 
 enum class LogFormat {
     Unspecified = 0,
-    Standard = 1,
-    JSON = 2
+    Standard = 1
+    // Todo: JSON log dev sau.
 };
 
 struct LogConfig {
     std::string name;
     LogFormat format = LogFormat::Standard;
     std::string logFilePath;
-    int logRotateDuration = 24 * 60 * 60; // 1 ngày
-    int logRotateBytes = 12 * 1024 * 1024; // 12MB mặc định
+    int logRotateDuration = 24 * 60 * 60;   // 1 ngày
+    int logRotateBytes = 12 * 1024 * 1024;  // 12MB
     int logRotateMaxFiles = 5;
     std::string defaultFileName = "kallisto.log";
     // Constructor khởi tạo LogConfig nếu muốn dùng
-    LogConfig(std::string_view fileName) {
+    LogConfig(std::string_view fileName = "") {
         if (fileName.empty()) {
-            throw std::invalid_argument("default file name is required");
+            throw std::invalid_argument("[error] (LogConfig) (constructor): default file name is required.");
         }
         defaultFileName = fileName;
     }
